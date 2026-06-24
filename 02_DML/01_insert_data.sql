db0d2a58b06a2f6f386cb029f68181e1ee731797
Sana Venkata Yasaswini <133481869+2001Yasaswini@users.noreply.github.com>
2026-06-24 12:44:10 +0530

    Add sample movie data
    Added sample movie records using INSERT statements for SQL practice.

---
+++ 02_DML/01_insert_data.sql
@@ -0,0 +1,10 @@
+-- Insert movie records
+
+INSERT INTO MVLST
+VALUES (1, 'Peddi', 300000000, 450000000, 'Ram Charan', 'Janhvi Kapoor');
+
+INSERT INTO MVLST
+VALUES (2, 'Bahubali', 1800000000, 6500000000, 'Prabhas', 'Anushka');
+
+INSERT INTO MVLST
+VALUES (3, 'Tulasi', 250000000, 400000000, 'Venkatesh', 'Nayanthara');
