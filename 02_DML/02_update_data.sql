8b52a3b8eb518843e266e57162719c00921466f8
Sana Venkata Yasaswini <133481869+2001Yasaswini@users.noreply.github.com>
2026-06-24 12:49:37 +0530

    Add UPDATE statement examples
    Added SQL UPDATE statement examples to modify existing movie records.

---
+++ 02_DML/02_update_data.sql
@@ -0,0 +1,17 @@
+-- Update movie director
+
+UPDATE MVLST
+SET Director_Name = 'Rajamouli'
+WHERE ID = 2;
+
+-- Update movie rating
+
+UPDATE MVLST
+SET Rating = 10
+WHERE ID = 2;
+
+-- Update movie ID
+
+UPDATE MVLST
+SET ID = 6
+WHERE Name = 'Tulasi';
