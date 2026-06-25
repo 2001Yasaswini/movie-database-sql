f34a28e4818e4c73c1ae6e059de7a91353ed8ae9
Sana Venkata Yasaswini <133481869+2001Yasaswini@users.noreply.github.com>
2026-06-25 17:35:02 +0530

    Add ORDER BY examples
    Added SQL ORDER BY examples for sorting movie records by rating, name and collection.

---
+++ 04_FILTERING/05_order_by.sql
@@ -0,0 +1,17 @@
+-- Sort movies by rating (highest first)
+
+SELECT *
+FROM CINEMA
+ORDER BY RATING DESC;
+
+-- Sort movies by name (A to Z)
+
+SELECT *
+FROM CINEMA
+ORDER BY NAME ASC;
+
+-- Sort movies by collection (highest first)
+
+SELECT *
+FROM CINEMA
+ORDER BY COLLECTION DESC;
