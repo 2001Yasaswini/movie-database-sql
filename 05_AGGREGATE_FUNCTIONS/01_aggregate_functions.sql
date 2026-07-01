142f4b4d7dd7a7329143548f17070a065b7a27fc
Sana Venkata Yasaswini <133481869+2001Yasaswini@users.noreply.github.com>
2026-07-01 13:10:27 +0530

    Add aggregate function examples
    Added examples of Oracle SQL aggregate functions including COUNT, SUM, AVG, MIN, and MAX.
    
    These queries demonstrate how to perform basic calculations and summarizations on data stored in the CINEMA table as part of the Movie Database SQL learning project.

---
+++ 05_AGGREGATE_FUNCTIONS/01_aggregate_functions.sql
@@ -0,0 +1,29 @@
+-- COUNT Function
+-- Count total movies
+
+SELECT COUNT(*)
+FROM CINEMA;
+
+-- MAX Function
+-- Highest rating
+
+SELECT MAX(RATING)
+FROM CINEMA;
+
+-- MIN Function
+-- Lowest rating
+
+SELECT MIN(RATING)
+FROM CINEMA;
+
+-- AVG Function
+-- Average rating
+
+SELECT AVG(RATING)
+FROM CINEMA;
+
+-- SUM Function
+-- Total collection
+
+SELECT SUM(COLLECTION)
+FROM CINEMA;
