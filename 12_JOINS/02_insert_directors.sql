6770f6a87d34078893f07906bcec79ae7ff38113
Sana Venkata Yasaswini <133481869+2001Yasaswini@users.noreply.github.com>
2026-07-05 19:55:32 +0530

    Insert director records
    Added sample director records to the DIRECTORS table for Oracle SQL join practice.

---
+++ 12_JOINS/02_insert_directors.sql
@@ -0,0 +1,9 @@
+INSERT INTO DIRECTORS VALUES (1,'BucchibabuSana','Indian');
+
+INSERT INTO DIRECTORS VALUES (2,'Rajamouli','Indian');
+
+INSERT INTO DIRECTORS VALUES (3,'VigneshRaja','Indian');
+
+INSERT INTO DIRECTORS VALUES (4,'Kranthi Madhav','Indian');
+
+INSERT INTO DIRECTORS VALUES (5,'Krishna Vamshi','Indian');
